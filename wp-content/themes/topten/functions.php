11f0a376421ace54ecc0ed6fcccdfd284e46ed02
CeciliaM <cecilia.merouche@gmail.com>
2016-02-02 09:47:39 +0100

    function.php

--- wp-content/themes/topten/functions.php
+++ wp-content/themes/topten/functions.php
@@ -48,4 +48,45 @@ require_once( 'library/sticky-posts.php' );
 /** If your site requires protocol relative url's for theme assets, uncomment the line below */
 // require_once( 'library/protocol-relative-theme-assets.php' );
 
-?>
+// --> Oeuvres
+         $labels_oeuvres_post_type_array = array(
+             'name'                  =>  __('Oeuvres', 'munterlinden'),
+             'singular_name'         =>  __('Oeuvre', 'munterlinden'),
+             'add_new'               =>  __('Ajouter une oeuvre', 'munterlinden'),
+             'add_new_item'          =>  __('Ajouter une oeuvre', 'munterlinden'),
+             'edit_item'             =>  __('Editer l\'oeuvre', 'munterlinden'),
+             'new_item'              =>  __('Nouvelle oeuvre', 'munterlinden'),
+             'view_item'             =>  __('Visualiser l\'oeuvre', 'munterlinden'),
+             'search_items'          =>  __('Rechercher une oeuvre', 'munterlinden'),
+             'not_found'             =>  __('Aucune oeuvre', 'munterlinden'),
+             'not_found_in_trash'    =>  __('Aucune oeuvre dans la corbeille', 'munterlinden'),
+             'parent_item_colon'     =>  __('--', 'munterlinden'),
+             'menu_name'             =>  __('Oeuvres', 'munterlinden')
+         );
+         $supports_oeuvres_post_type_array = array(
+             'title',
+             'editor',
+             'thumbnail',
+             'excerpt'
+         );
+         $rewrite_oeuvres_post_type_array = array(
+             'slug'          =>  _x('collections', 'Pour les collections', 'foundationpress'),
+             'with_front'    =>  false,
+             'feeds'         =>  true,
+             'pages'         =>  true
+         );
+         $args_oeuvres_post_type_array = array(
+             'labels'                =>  $labels_oeuvres_post_type_array,
+             'description'           =>  __('Contenu complet Projets', 'munterlinden'),
+             'public'                =>  true,
+             'publicly_queryable'    =>  true,
+             'menu_position'         =>  50,
+             'menu_icon'             =>  '',
+             'capability_type'       =>  'post',
+             'supports'              =>  $supports_oeuvres_post_type_array,
+             'rewrite'               =>  $rewrite_oeuvres_post_type_array,
+             'show_in_nav_menus'     =>  true,
+             'hierarchical'          =>  true,
+             'has_archive'           =>  true
+         );
+         register_post_type('oeuvres', $args_oeuvres_post_type_array);
